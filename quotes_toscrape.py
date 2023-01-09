84317ceef5eb4d4358a0972ae6f9ec53955aa23d
mehedicse133 <mehedicse047@gmail.com>
2023-01-09 22:32:20 +0600

    add dependensies

---
+++ quotes_toscrape.py
@@ -0,0 +1,11 @@
+import time
+import getpass
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(service=ChromeService(
+    ChromeDriverManager().install()))
